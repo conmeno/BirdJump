d006985deb7669fd93d0724327f935d3f0bf008a
conmeno <comeno@outlook.com>
2017-06-25 23:17:26 +0700

    ff

--- UberJump/AdManager/Data.swift
+++ UberJump/AdManager/Data.swift
@@ -13,35 +13,27 @@ class Data {
     //5120 game
     
     //URL setup online
-    let AdURL: String = "http://webappchannel.com/tonyapp/baby-bird-v4.txt"
+     let AdURL: String = "http://webappchannel.com/v5/baby_bird_jump_v5.txt"
     
     //google
-    let gBanner : String = "ca-app-pub-4291954185603132/5889075604"
+    let gBanner : String = ""
     let gFull : String = ""
-    let TestDeviceID: String = "85dc971ff2c5b4d2ebc796bd849c48a7"
+    let TestDeviceID: String = ""
     
 
     //chartboost
     let cAppID: String = ""
     let cSign : String = ""
     //amazon
-    let AmazonKey: String = "5449470a14414b40a25f45a1fb7ebf3d"
+    let AmazonKey: String = ""
     
     //adcolony
-    let AdcolonyAppID: String = "app5541e30d9e4f4727af"
-    let AdcolonyZoneID: String = "vzda0a261d16c9409891"
+    let AdcolonyAppID: String = ""
+    let AdcolonyZoneID: String = ""
     
     //revmob appID
-    let RevmobID: String = "5745696880b3111731c69162"
-    
-    //Vungle ID
-    let VungleID: String = ""
-    
-    let UnityGameId: String = ""
-    
-    let SonicID: String = "487f724d"
-    
-    
+    let RevmobID: String = ""
+       
     
     
     
